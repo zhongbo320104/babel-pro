1df9d7af75cb39b5a9b796d592e5f76e255b28f1
zhongbo320104 <3279028951@qq.com>
2022-02-23 21:52:23 +0800

    fix: 添加测试文件

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
-const a = 3
+let line = "asdfasdfo"
+let result, map = new Map()
+for (let i = 0; i < line.length; i++) {
+    if (!map.has(line[i])) {
+        map.set(line[i], i)
+    } else {
+        map.set(line[i], -1)
+    }
+}
+map.forEach(item => {
+    console.log(item)
+})
